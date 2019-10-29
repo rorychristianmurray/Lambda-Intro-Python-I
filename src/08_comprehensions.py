fc94084659d5044911d2569a56a185c5596eb56d
Rory Murray <rory.christian.murray@gmail.com>
2019-10-29 18:06:11 -0400

    Comprehensions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,32 +10,50 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+arr = [2, 3, 4, 5, 6]
+y = [x for x in range(1, 6)]
+z = [x-1 for x in arr]
+w = list(map(lambda x: x, range(1, 6)))
 
-print (y)
+print("y:", y)
+print("z:", z)
+print("w:", w)
 
+
+a = ["Mali", "Rory", "Nate", "York", "Dustin", "Jamie", "Crawford", "Javontay"]
+for i in a[4:]:
+    print(f"We have {i} left to go")
+
+m = [i for i in a[4:]]
+print(f"We have{m} to go")
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [x**3 for x in range(9)]
+z = list(map(lambda x: x**3, range(9)))
 
-print(y)
+print("cubes y:", y)
+print("cubes z:", z)
+
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [x.upper() for x in a]
 
-print(y)
+print("upper:", y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
-x = input("Enter comma-separated numbers: ").split(',')
+# x = input("Enter comma-separated numbers: ").split(',')
+
+l = list([1, 9, 27, 3, 147, 21948932, 238, 54, 2, 3, 54535, 23344, 543543])
+print("l", l)
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [x for x in l if x % 2 == 0]
 
 print(y)
