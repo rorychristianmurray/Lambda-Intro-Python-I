33cae6e938f66e57e731799859d8792267dd31f4
Rory Murray <rory.christian.murray@gmail.com>
2019-10-30 11:37:23 -0400

    Args

--- src/11_args.py
+++ src/11_args.py
@@ -33,18 +33,20 @@ a = [7, 6, 5, 4]
 # What thing do you have to add to make this work?
 
 
-def f2a(*args):
-    for arg in args:
-        print("f2a arg:", arg)
-
-
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+
+
+def f3(arg1, arg2=1):
+    # if one argument, return that value plus one
+    # if two args, return the sum of arguments
+    return arg1 + arg2
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -59,6 +61,10 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for k, v in kwargs.items():
+        print(f"key: {k}, value: {v}")
+
 
 # Should print
 # key: a, value: 12
@@ -77,4 +83,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
