7ea097643a4e916e6a82a9120fff793f8bccf2c5
Rory Murray <rory.christian.murray@gmail.com>
2019-10-30 16:28:47 -0400

    File IO

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,9 +10,24 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open('foo.txt') as f:
+    global read_data
+    read_data = f.read()
+
+
+print(read_data)
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+b = open('bar.txt', "w")
+b.write("I bite my thumb at you sir!\n")
+b.write("I bite my thumb at you!\n")
+b.write("R U listening?\n")
+b.close()
+
+# print(b.read())
