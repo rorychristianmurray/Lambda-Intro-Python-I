dd24401dc0159047aa8fecf3dc7242d55c052400
Rory Murray <rory.christian.murray@gmail.com>
2019-10-29 20:07:45 -0400

    Functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,11 +2,23 @@
 
 # YOUR CODE HERE
 
+
+def is_even(numarg):
+    if numarg % 2 == 0:
+        True
+        print("true")
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
+print(num)
+
+if num % 2 == 0:
+    print("Even!")
+else:
+    print("Odd!")
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
